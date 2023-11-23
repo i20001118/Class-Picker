6c511d1a456a07ef466b18b0a714975cec374dca
TESTER_01 <38677477+king9487@users.noreply.github.com>
2023-11-23 14:40:59 +0000

    Main

--- AddElectiveCourses.py
+++ AddElectiveCourses.py
@@ -1,62 +1,55 @@
+import pandas as pd
+import json
 
 #讀入所有課程
 def read_course_info():
-    course_info = []  # 保存課程信息的列表
+    filename = 'Course.json'
+    dict_course = {}  # 保存課程信息的列表
 
     try:
-        with open("Course.txt", "r") as file:
-            for line in file:
-                # 解析每一行，以逗號分隔
-                course_id, course_name, course_credit, course_time, course_remainder = map(str.strip, line.split(','))
-
-                # 將課程信息組成一個字典，加入列表中
-                course = {
-                    "course_id": course_id,
-                    "course_name": course_name,
-                    "course_credit": course_credit,
-                    "course_time": course_time,
-                    "course_remainder": course_remainder
-                }
-                course_info.append(course)
-
-        return course_info
+        with open(filename, 'r') as f:
+            dict_course = json.load(f)
+        f.close()
+        return dict_course
     except FileNotFoundError:
-        print("ERROR: 找不到Course.txt檔案。")
+        print("ERROR: 找不到 " + filename + " 檔案。")
         return None
 
 # 讀入學生課表資料
 def get_curriculum(student_id):
-    filename = student_id + ".txt"
-    curriculum_info = []
+    filename = student_id + ".json"
+    curriculum_info = {}
 
     try:
-        with open(filename, "r") as file:
-            for line in file:
-                course_id, course_credit,course_time = map(str.strip, line.split(','))
-                curriculum = {
-                    "course_id": course_id,
-                    "course_credit": course_credit,
-                    "course_time": course_time
-                }
-                curriculum_info.append(curriculum)
+        with open(filename, 'r') as f:
+            curriculum_info = json.load(f)
+        f.close()
         return curriculum_info
-    
     except FileNotFoundError:
-        print("ERROR: 找不到" + student_id + "檔案")
+        print("ERROR: 找不到 " + filename + " 檔案。")
+        return None
 
-def write_curriculum(student_id, course_id, course_credit, course_time):
-    filename = student_id + ".txt"
+def search_course(course_id):
+    course_info = read_course_info()
+    return course_info[course_id]
+
+def write_curriculum(student_id, course_id):
+    filename = student_id + ".json"
 
     try:
-        with open(filename, "a") as file:
-            line = [course_id, course_credit, course_time]
-            file.writelines(line)
-            file.close()
-        return True
+        course_info = search_course(course_id)
+        if (verify(student_id, course_info['time'])):
+            with open(filename, "w") as f:
+                json.dump(course_info, f, indent = 2)
+            return True
     except FileNotFoundError:
         print("ERROR: " + student_id +"檔案不存在")
         return False
 
 def verify(student_id, course_time):
     curriculum = get_curriculum(student_id)
-    
+    for key, data in curriculum:
+        if data['time'] == course_time:
+            return False
+    return True
+
